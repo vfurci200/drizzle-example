fe7c8b044ec46cc42b1bf0e5417a9b8bdde38d5e
Vince <10850139+vfurci200@users.noreply.github.com>
2021-04-06 17:35:37 +0100

    initial commit2

---
+++ client/src/drizzle/drizzleOptions.js
@@ -0,0 +1,8 @@
+import SimpleStorage from "../contracts/SimpleStorage.json";
+const options = {
+   contracts: [SimpleStorage],
+   events: {
+      SimpleStorage: ["DataChanged"],
+   },
+};
+export default options;
---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract SimpleStorage {
+  string public data = "hello";
+  event DataChanged(string indexed data);
+  function setData(string calldata _data) external {
+     data = _data;
+     emit DataChanged(_data);
+  }
+}
